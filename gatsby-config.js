a67690fe20f354f5c0c42c527f7f8de82c6506ff
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2018-12-18 21:14:47 +0700

    edit title and desc blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'Muhammad Wafaul Himam Blog',
+    description: 'This is my personal Blog.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
